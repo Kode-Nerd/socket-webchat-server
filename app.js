740819a45fc3eab060fdb0ace06893b18aed995b
Muhammad Ramadhoni <ramadhoni@keyreply.com>
2020-09-02 14:15:36 +0700

    added namespace

--- app.js
+++ app.js
@@ -13,10 +13,39 @@ const log = require(__dirname + "/helpers/log");
 
 const PORT = process.env.PORT || 80;
 
-app.use(cors());
-app.use("/webchat", express.static(path.join(__dirname, "public")));
+const socketNamespace = io.of("/socket");
 
-io.on("connect", (socket) => {
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.static(path.join(__dirname, "public")));
+app.post('/admin/broadcast', (req, res, next) => {
+  const { text } = req.body;
+  const message = {
+    type: "text",
+    sender: "admin",
+    text,
+    created: new Date().getTime()
+  }
+  log(message);
+  socketNamespace.emit("message", message);
+
+  res.status(200).send({ message });
+})
+app.post('/admin/typing', (req, res, next) => {
+  const { setting } = req.query;
+  const event = setting === "true" ? true : setting === "false" ? false : false;
+  const typing = {
+    user: "admin",
+    event
+  }
+  log({ typing });
+  socketNamespace.emit("typing", typing);
+
+  res.status(200).send({ typing });
+})
+
+socketNamespace.on("connect", (socket) => {
   log("a user connected", socket.id);
   const event = {
     type: "event",
@@ -25,7 +54,7 @@ io.on("connect", (socket) => {
     created: socket.handshake.issued
   }
   log(event);
-  io.emit("message", event);
+  socketNamespace.emit("message", event);
 
   socket.on("message", (text) => {
     const message = {
@@ -35,7 +64,7 @@ io.on("connect", (socket) => {
       created: socket.handshake.issued
     }
     log(message);
-    io.emit("message", message);
+    socketNamespace.emit("message", message);
   })
 
   socket.on("typing", (event) => {
@@ -56,7 +85,7 @@ io.on("connect", (socket) => {
       created: socket.handshake.issued
     }
     log(event);
-    io.emit("message", event);
+    socketNamespace.emit("message", event);
   })
 })
 
